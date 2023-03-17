8221f080920005ec58b65ecb6a907de9fe94eae2
asalamatov <azamat.salamatov2004@gmail.com>
2023-03-17 07:57:56 -0500

    first jquery is done

--- FrontEnd/JQUERY/sample/js/script.js
+++ FrontEnd/JQUERY/sample/js/script.js
@@ -1,11 +1,11 @@
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $("p").hide();
-//     })
-// });
-
 $(document).ready(function(){
     $("button").click(function(){
-        $(".center h1").hide();
-    })
+        $("p:first").css("color", "yellow");
+    });
 });
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $(".center h1").hide();
+//     })
+// });
